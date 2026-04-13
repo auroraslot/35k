07e35d2aafb50bffd8a28a5a2b6680b2a350b9c5
tangweize <tangweize@pailifan.com>
2026-04-13 18:41:15 +0800

    【阅读题解】移除元素-快慢指针

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        // 给一个非严格递增的数字，要求：1、对该数组去重；2、返回去重后的数组长度；
-        // 例如
-        int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        // 给你一个数组和一个target，要求：原地移除等于target的元素；修改数组，使其前n个元素是不等于target的元素。
+        // 例如target = 3
+        int[] arr = new int[]{3, 2, 2, 3};
         // 结果：
-        // 长度=5
-        // 新数组=[0,1,2,3,4,_,_,_,_,_]
+        // 数量=2
+        // 新数组=[2,2,_,_]
 
-        int length = removeDuplicates(arr);
+        int length = removeElement(arr, 3);
         System.out.println(length);
         for (int i : arr) {
             System.out.println(i);
@@ -18,27 +18,29 @@ class Solution {
     }
 
 
-    public static int removeDuplicates(int[] nums) {
-        // 两个指针
-
-        // 慢指针：记录不相等的元素应该存放的位置
+    // 快慢指针
+    public static int removeElement(int[] nums, int val) {
+        // 慢指针：需要保留的位置
         int s = 0;
         // 快指针：向前扫描
-        int f = 1;
+        int f = 0;
 
-        // 快慢指针元素不相等，说明遇到了新元素，慢指针右移后替换元素
-        // 如果相等则忽略
-        // 快指针每次循环都继续向前扫描
-        // 最后的有效长度就是慢指针+1
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[s] != nums[f]) {
-                s++;
+        // 遍历数组
+        for (int i = 0; i < nums.length; i++) {
+            // 快指针是否等于val
+            if (nums[f] != val) {
+                // 不相等
+                // 快指针元素覆盖到慢指针
                 nums[s] = nums[f];
+                // 慢指针右移
+                s++;
             }
 
+            // 每次循环快指针右移
             f++;
         }
-        return s + 1;
+
+        // 返回慢指针
+        return s;
     }
 }
