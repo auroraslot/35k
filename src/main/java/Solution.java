cfcbb91f916574c17fe0e25b2f436c3c47c0bdad
tangweize <tangweize@pailifan.com>
2026-04-14 11:47:44 +0800

    【阅读题解】合并两个有序数组

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,43 +3,69 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        // 给一个非严格递增的数字，要求：1、对该数组去重；2、返回去重后的数组长度；
+        // 有两个非递减的数组nums1和nums2
+        // 请合并nums2到nums1，使合并后的数组保持非递减排序
+        // nums1的有效元素为m，nums2的有效元素为n。m + n = nums1.length
         // 例如
-        int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = new int[]{2, 5, 6};
+        int n = 3;
         // 结果：
-        // 长度=5
-        // 新数组=[0,1,2,3,4,_,_,_,_,_]
+        // nums1=[1, 2, 2, 3, 5, 6]
 
-        int length = removeDuplicates(arr);
-        System.out.println(length);
-        for (int i : arr) {
+        for (int i : nums1) {
+            System.out.println(i);
+        }
+        merge(nums1, m, nums2, n);
+
+        System.out.println("===========");
+
+        for (int i : nums1) {
             System.out.println(i);
         }
     }
 
 
-    // 快慢指针
-    public static int removeDuplicates(int[] nums) {
-        // 慢指针：指向现在保留元素的位置
-        int s = 0;
-        // 快指针：向前扫描
-        int f = 1;
+    // 提示1：如果从前往后填，nums1 前面的有效元素会被覆盖。那反过来呢？
+    // 提示2：每次比较两个数组的最大值，放到 nums1 的最末尾，指针逐步前移
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 解法一：暴力 — 合并后排序（不推荐）
 
-        // 遍历数组
-        for (int i = 0; i < nums.length - 1; i++) {
-            // 快慢指针是否相等
-            // 不相等，说明是需要保留的新元素
-            if (nums[f] != nums[s]) {
-                // 慢指针右移
-                s++;
-                // 快指针元素覆盖到慢指针位置
-                nums[s] = nums[f];
+        // 解法二：逆向双指针
+        // 指针一：指向nums1的有效尾端
+        int p1 = m - 1;
+        // 指针二：指向nums2的有效尾端
+        int p2 = n - 1;
+        // 指针三：指向nums1的尾端
+        int p = nums1.length - 1;
+
+        // 从右向左遍历数组
+        // 直到任意一个指针<0退出循环
+        while (p1 >= 0 && p2 >= 0) {
+            // 指针一和指针二比较大小
+            if (nums1[p1] >= nums2[p2]) {
+                // 更大的元素移动到指针三
+                nums1[p] = nums1[p1];
+                // 大元素指针左移
+                p1--;
+            } else {
+                nums1[p] = nums2[p2];
+                p2--;
             }
-            // 每次循环快指针右移
-            f++;
+
+            // 每次指针三左移
+            p--;
         }
 
-        // 返回慢指针
-        return s + 1;
+        // 如果nums2还有剩余，搬过去
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
+        }
+
+        // 如果是nums1还有剩余，不用管，因为已经在原位了，本身就是有序的
+
     }
 }
