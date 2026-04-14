55c2e524e215a4df1fde377aafb8636528ff64a7
tangweize <tangweize@pailifan.com>
2026-04-14 13:56:33 +0800

    【阅读题解】53. 最大子数组和

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -18,63 +18,25 @@ class Solution {
 
 
     public static int maxSubArray(int[] nums) {
-        // 如何界定连续
-        // 需要知道该元素是否有出现过
-        // 如果出现过，则子数组中断，记录该子数组最大和
+        // dp[i] = max(dp[i - 1] + nums[i], nums[i])
 
-        // 两个指针，左指针和右指针
-        // 左右指针起点相等
-        // int l = 0;
-        // int r = 0;
+        // 要拿到最大的，我要不要加上之前的数组的和？
 
-        // 一个结构，保存出现过的元素字典
-        Set<Integer> map = new HashSet<>(nums.length);
+        int sum = nums[0];
+        int dpSum = nums[0];
 
-        // 两个变量，历史最大和，当前和
-        int cSum = 0;
-        int hSum = 0;
+        for (int i = 1; i < nums.length; i++) {// -2, 1, -3, 4, -1, 2, 1, -5, 4
 
-        for (int i = 0; i < nums.length; i++) {
-            int r = i;
-            // 一直处理到右指针到底为止
-            while (r < nums.length) {// -2, 1, -3, 4, -1, 2, 1, -5, 4
-                // 右指针往右扫描
-
-                // 右指针指向元素是否出现过
-                if (map.contains(nums[r])) {
-                    // 如果出现过，子数组中断
-
-                    // 如果当前和>历史最大和
-                    if (cSum > hSum) {
-                        // 覆盖历史最大和
-                        hSum = cSum;
-                        // 清除当前和
-                        cSum = 0;
-                    }
-                    // 清除结构
-                    map.clear();
-                    // 左指针移动到右指针
-                    // l = r;
-                } else {
-                    // 如果没出现过
-
-                    // 加到当前和
-                    cSum = cSum + nums[r];
-                    // 保存到结构
-                    map.add(nums[r]);
-                    r++;
-                }
-
-
+            if (dpSum < 0) {
+                dpSum = 0;
             }
 
-            hSum = Math.max(cSum, hSum);
-            cSum = 0;
-            map.clear();
+            dpSum = dpSum + nums[i];
+            sum = Math.max(sum, dpSum);
 
         }
 
-        // 返回历史和
-        return hSum;
+
+        return sum;
     }
 }
