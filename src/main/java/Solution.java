5a97c6571bd76cb8ab20ba467d4db34fd4a42733
tangweize <tangweize@pailifan.com>
2026-04-14 10:04:06 +0800

    【阅读题解 + 重刷2】删除有序数组中的重复项

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        // 给你一个数组和一个target，要求：原地移除等于target的元素；修改数组，使其前n个元素是不等于target的元素。
-        // 例如target = 3
-        int[] arr = new int[]{3, 2, 2, 3};
+        // 给一个非严格递增的数字，要求：1、对该数组去重；2、返回去重后的数组长度；
+        // 例如
+        int[] arr = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         // 结果：
-        // 数量=2
-        // 新数组=[2,2,_,_]
+        // 长度=5
+        // 新数组=[0,1,2,3,4,_,_,_,_,_]
 
-        int length = removeElement(arr, 3);
+        int length = removeDuplicates(arr);
         System.out.println(length);
         for (int i : arr) {
             System.out.println(i);
@@ -19,28 +19,27 @@ class Solution {
 
 
     // 快慢指针
-    public static int removeElement(int[] nums, int val) {
-        // 慢指针：需要保留的位置
+    public static int removeDuplicates(int[] nums) {
+        // 慢指针：指向现在保留元素的位置
         int s = 0;
         // 快指针：向前扫描
-        int f = 0;
+        int f = 1;
 
         // 遍历数组
-        for (int i = 0; i < nums.length; i++) {
-            // 快指针是否等于val
-            if (nums[f] != val) {
-                // 不相等
-                // 快指针元素覆盖到慢指针
-                nums[s] = nums[f];
+        for (int i = 0; i < nums.length - 1; i++) {
+            // 快慢指针是否相等
+            // 不相等，说明是需要保留的新元素
+            if (nums[f] != nums[s]) {
                 // 慢指针右移
                 s++;
+                // 快指针元素覆盖到慢指针位置
+                nums[s] = nums[f];
             }
-
             // 每次循环快指针右移
             f++;
         }
 
         // 返回慢指针
-        return s;
+        return s + 1;
     }
 }
