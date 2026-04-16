6b70fdec56d297675e3a88b4a6662d7d25f54352
tangweize <tangweize@pailifan.com>
2026-04-16 14:03:11 +0800

    【阅读题解】3. 无重复字符的最长子串

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,3 +1,4 @@
+import sun.plugin.net.protocol.jar.CachedJarURLConnection;
 import utils.ArrayUtils;
 
 import java.util.*;
@@ -7,48 +8,81 @@ import static utils.ArrayUtils.printArray;
 class Solution {
 
     public static void main(String[] args) {
-        //编写一个函数来查找字符串数组中的最长公共前缀。
-        //
-        //如果不存在公共前缀，返回空字符串 ""。
+        //给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
         //
         //
         //
-        //示例 1：
+        //示例 1:
         //
-        //输入：strs = ["flower","flow","flight"]
-        //输出："fl"
+        //输入: s = "abcabcbb"
+        //输出: 3
+        //解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。注意 "bca" 和 "cab" 也是正确答案。
 
-        // String[] strs = new String[]{"flower", "flow", "flight"};
-        String[] strs = new String[]{"ab", "a"};
-        String s = longestCommonPrefix(strs);
-        System.out.println(s);
+//         String s = "abcabcbb";
+//         String s = "aab";
+//        String s = "dvdf";
+        String s = "ppwppwcww";
+//        String s = "aabaab!bb";
+
+        System.out.println(lengthOfLongestSubstring(s));
     }
 
 
-    public static String longestCommonPrefix(String[] strs) {
-        // 最长公共前缀
+    public static int lengthOfLongestSubstring(String s) {
+        // 无重复字符的最长子串
 
-        StringBuilder sb = new StringBuilder();
+        // 滑动窗口
 
-        // 遍历字符
-        for (int i = 0; i < strs[0].length(); i++) {
-            // 遍历数组
-            for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length()) {
-                    return sb.toString();
-                }
+        // 左指针：窗口尾端
+        int l = 0;
+        // 右指针：窗口首端
+//        int r = 0;
 
-                // 字符是否不相等
-                if (strs[j].charAt(i) != strs[0].charAt(i)) {
-                    // return公共子串
-                    return sb.toString();
-                }
+        // 哈希表：字符是否出现过
+        Set<Character> map = new HashSet<>();
+
+        // 当前长度
+        int cH = 0;
+        // 最大长度
+        int hH = 0;
+
+        // 右指针扫描
+        char[] charArray = s.toCharArray();
+        for (int r = 0; r < charArray.length; r++) {
+            char c = charArray[r];
+
+            // 字符出现过
+            while (map.contains(c)) {
+                // 更新最大长度
+                hH = Math.max(hH, cH);
+                // 移动过程中的元素从哈希表中移除
+                map.remove(charArray[l]);
+                l++;
+                // 当前长度-
+                cH--;
+
+                // 左指针右移，一直右移到重复字符所在的位置
+//                while (c != charArray[l]) {
+//
+//
+//                }
             }
 
-            // 相等字符记录公共子串
-            sb.append(strs[0].charAt(i));
+//            else {
+                // 没有出现过
+                // 更新当前长度
+                cH++;
+                // 添加到哈希表
+                map.add(charArray[r]);
+//            }
+
         }
 
-        return sb.toString();
+
+
+        // 返回最大长度
+
+        return Math.max(hH, cH);
+
     }
 }
